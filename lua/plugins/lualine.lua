d349ca6819b7c831e6834be6f0c14b7a54579673
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-28 22:07:17 -0500

    Update lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,5 +2,17 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+    		theme = "auto",
+    		disabled_filetypes = { 'NvimTree', 'TelescopePrompt' },
+				always_divide_middle = true,
+				refresh = {
+					statusline = 500,
+				}
+			},
+			sections = {}
+		})
   end
 }
