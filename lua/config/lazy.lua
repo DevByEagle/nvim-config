13caed79c60f9cd3a7cf901cf7fa1ec69a514f05
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-28 20:55:54 -0500

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,1 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" }
+	},
+	ui = {
+		border = "rounded",
+    title = "Plugin Manager",
+    title_pos = "center",
+	},
+	rocks = {
+		enabled = false,
+		hererocks = false,
+	},
+	-- Colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- Automatically check for plugin updates
+  checker = { enabled = true },
+})
 
