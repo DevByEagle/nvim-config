8781a0ba2294ec286b714ddf9e58b2d1eb87935c
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-28 21:01:16 -0500

    Create nvim-notify.lua

---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,15 @@
+return {
+	"rcarriga/nvim-notify",
+	event = "VeryLazy",
+	config = function()
+		vim.notify = require("notify")
+		vim.notify.setup {
+			-- Animation style
+			stages = "fade_in_slide_out",
+			-- Default timeout for notifications
+			timeout = 1500,
+			background_colour = "#2E3440",
+		}
+	end
+}
+
