21a12a386311c170561542ed584f241f68c1aa56
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-02-28 21:00:23 -0500

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,18 @@
+local opt = vim.opt
 
+-- Line numbers
+opt.number = true
+
+-- Tabs & Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+
+-- Appearance
+opt.termguicolors = true
+opt.showmode = false
+vim.cmd("colorscheme onedark")
+
+-- Performance
+opt.updatetime = 250
+opt.timeoutlen = 400
